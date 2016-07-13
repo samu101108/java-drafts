ca191772dffb755d0519447cb8c7ec1b7e8119e8
Samu101108 <samu101108@users.noreply.github.com>
2016-07-12 22:14:16 -0300

    Update body.java

--- body.java
+++ body.java
@@ -1,11 +1,23 @@
-//Bugdroid in Grafic Java Custom Lib from Udacity
+//Bugdroid in Graphic Java Custom Lib from Udacity
 
 //head
+/*
+*I can draw the head with a circle and to "cut" half off I could put a Rectangle with 
+*the same color of the background above the circle that makes the head.
+*/
+
 //eyes
+
 //antennas
 
 //arms
+/*
+*Put a circle at the edges of the rectangle that makes the arms.
+*/
 
 //trunk
 
 //legs
+/*
+*Put a circle at the edges of the rectangle that makes the feett. 
+*/
